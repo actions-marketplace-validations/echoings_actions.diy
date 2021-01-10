559d1f0254a925e4a1572ebde2001d0bde0bb2db
zeroooooooo <abc.xyz.2be+github@gmail.com>
2021-01-11 00:44:58 +0800

    feat: add fs as dev

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import * as glob from '@actions/glob';
 import * as io from '@actions/io';
 import * as toolCache from '@actions/tool-cache';
 import axios from 'axios';
+import fs from 'fs-extra';
 
 import DIY from './diy';
 
@@ -28,6 +29,7 @@ async function run(): Promise<void> {
         axios,
         core,
         github,
+        fs,
         exec,
         glob,
         cache,
